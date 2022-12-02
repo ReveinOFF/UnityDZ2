ed417b7a8833d66d9cc94420fa05e401ac3c8aca
RonniePlay <57015640+RonniePlay@users.noreply.github.com>
2022-12-02 21:03:08 +0300

    DZ

--- My project/Assets/Scripts/Cloud.cs
+++ My project/Assets/Scripts/Cloud.cs
@@ -5,72 +5,61 @@ using UnityEngine;
 public class Cloud : MonoBehaviour
 {
     [Header("Set in Inspector")]
-    public int numClouds = 40;
-    public GameObject cloudPrefab;
-    public Vector3 cloudPosMin = new Vector3(-50, -5, 10);
-    public Vector3 cloudPosMax = new Vector3(150, 5, 10);
-    public float cloudScaleMin = 1;
-    public float cloudScaleMax = 3;
-    public float cloudSpeedmult = 0.5f;
+    public GameObject cloudSphere;
+    public int numSphereMin = 6;
+    public int numSpheresMax = 10;
+    public Vector3 sphereOffsetScale = new Vector3(5, 2, 1);
+    public Vector2 sphereScaleRangeX = new Vector2(4, 8);
+    public Vector2 sphereScaleRangeY = new Vector2(3, 4);
+    public Vector2 sphereScaleRangeZ = new Vector2(2, 4);
+    public float ScaleYMin = 2f;
 
-    private GameObject[] cloudeInstances;
-
-    private void Awake()
-    {
-        // Создать массив для хранения всех экземпляров облаков
-        cloudeInstances = new GameObject[numClouds];
-        // Найти родительский игровой объект Cloud
-        GameObject anchor = GameObject.Find("Cloud");
-        // Создать в цикле заданное количество облаков
-        GameObject cloud;
-        for (int i = 0; i < numClouds; i++)
-        {
-            // Создать экземпляр cloudPrefab
-            cloud = Instantiate<GameObject>(cloudPrefab);
-            // Выбрать местоположение для облака
-            Vector3 cPos = Vector3.zero;
-            cPos.x = Random.Range(cloudPosMin.x, cloudPosMin.y);
-            cPos.y = Random.Range(cloudPosMax.x, cloudPosMax.y);
-            // Масштабировать облако
-            float scaleU = Random.value;
-            float scaleVal = Mathf.Lerp(cloudScaleMin, cloudScaleMax, scaleU);
-            // Меньшие облака (с меньшим значением scaleU) должны быть ближе
-            // к земле
-            cPos.y = Mathf.Lerp(cloudPosMax.y, cPos.y, scaleU);
-            // Меньшие облака должны быть дальше
-            cPos.z = 100 - 90 * scaleU;
-            // Применить полученные значения координат и масштаба к облаку
-            cloud.transform.position = cPos;
-            cloud.transform.localScale = Vector3.one * scaleVal;
-            // Сделать облако дочерним по отношению к anchor
-            cloud.transform.SetParent(anchor.transform);
-            // Добавить облако в массив
-            cloudeInstances[i] = cloud;
-
-        }
-    }
+    private List<GameObject> spheres;
 
     // Start is called before the first frame update
     void Start()
     {
+        spheres = new List<GameObject>();
+
+        int num = Random.Range(numSphereMin, numSpheresMax);
+
+        for (int i = 0; i < num; i++)
+        {
+            GameObject sp = Instantiate<GameObject>(cloudSphere);
+
+            spheres.Add(sp);
+
+            Transform spTrans = sp.transform;
+            spTrans.SetParent(this.transform);
+
+            Vector3 offset = Random.insideUnitSphere;
+            offset.x *= sphereOffsetScale.x;
+            offset.y *= sphereOffsetScale.y;
+            offset.z *= sphereOffsetScale.z;
+            spTrans.localPosition = offset;
+
+            Vector3 scale = Vector3.one;
+            scale.x = Random.Range(sphereScaleRangeX.x, sphereScaleRangeX.y);
+            scale.y = Random.Range(sphereScaleRangeY.x, sphereScaleRangeY.y);
+            scale.z = Random.Range(sphereScaleRangeZ.x, sphereScaleRangeZ.y);
+
+            scale.y *= 1 - (Mathf.Abs(offset.x) / sphereOffsetScale.x);
+            spTrans.localScale = scale;
+        }
 
     }
 
-    // Update is called once per frame
     void Update()
     {
-        foreach (GameObject cloud in cloudeInstances)
+    }
+
+    void Restart()
+    {
+        foreach (GameObject sp in spheres)
         {
-            float scaleVal = cloud.transform.localScale.x;
-            Vector3 cPos = cloud.transform.position;
-            cPos.x -= scaleVal * Time.deltaTime * cloudSpeedmult;
-
-            if (cPos.x <= cloudPosMin.x)
-            {
-                cPos.x = cloudPosMax.x;
-            }
-
-            cloud.transform.position = cPos;
+            Destroy(sp);
         }
+        Start();
     }
 }
+
